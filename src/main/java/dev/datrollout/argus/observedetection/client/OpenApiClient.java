93591c6289c6b502d2b8dbc5584543f7cb07dd90
Ngo Vu Minh Dat <ngovuminhdat@gmail.com>
2026-05-23 15:20:28 +0700

    deprecate observe-detection

--- src/main/java/dev/datrollout/argus/observedetection/client/OpenApiClient.java
+++
@@ -1,8 +0,0 @@
-package dev.datrollout.argus.observedetection.client;
-
-import java.util.Map;
-
-public interface OpenApiClient extends ObservabilityClient {
-
-    Map<String, Object> getSchema();
-}
--- src/main/java/dev/datrollout/argus/observedetection/probe/ProbeFactory.java
+++
@@ -1,9 +0,0 @@
-package dev.datrollout.argus.observedetection.probe;
-
-import dev.datrollout.argus.observedetection.model.ProbeTarget;
-import java.util.List;
-
-public interface ProbeFactory {
-
-    List<CapabilityProbe> probesFor(ProbeTarget target);
-}
